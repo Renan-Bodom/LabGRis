89cd37a41d02ae77fbdfced7d03b5af8512fed45
Renan-Bodom <renan1211@hotmail.com>
2021-03-25 11:00:32 -0300

    app users configurada

--- LabGRis/urls.py
+++ LabGRis/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'), name='url_main')
+    path('', include('main.urls'), name='url_main'),
+    path('users/', include('users.urls'), name='url_users')
 ]
