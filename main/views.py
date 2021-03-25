b245124e558e90e9ea91f9783b2dc0ec1e86f37c
Renan-Bodom <renan1211@hotmail.com>
2021-03-25 12:36:17 -0300

    Statics configurado

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
+    data = {}
+    #data['SessionUser'] = getSessionUser(request)
+    data['context'] = ""
     return render(request, 'main/main.html')
