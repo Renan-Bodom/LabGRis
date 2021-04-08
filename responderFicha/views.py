aeeb2d76a4cdbbf5a763d0d37504d43f4090fa04
Renan-Bodom <renan1211@hotmail.com>
2021-04-08 10:55:56 -0300

    Listado informações nas telas

--- responderFicha/views.py
+++ responderFicha/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from LabGRis.decorators import validate_session, getSessionUser
+from LabGRis.funcoesCompartilhadas import criarListaDoBanco, criarListaDoBancoKEY
+from LabGRis.pyrebase_settings import db
 
 
 @validate_session
@@ -7,5 +9,13 @@ def responderFicha(request):
     data = {}
     data['SessionUser'] = getSessionUser(request)
     data['context'] = ""
+
+    # Bancos
+    bancoModeloFicha = "Templates de Fichas"
+
+    #########  Busca Modelos de Ficha já cadastradas
+    fichaSalvas = db.child(bancoModeloFicha).get()
+    listaFicha = criarListaDoBanco(fichaSalvas)
+    data['listaFicha'] = listaFicha
 
     return render(request, 'responderFicha/responderFicha.html', data)
