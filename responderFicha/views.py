4b7458a0cc5c46411761c9fe358cb50abb0f63fc
Renan-Bodom <renan1211@hotmail.com>
2021-07-28 17:40:10 -0300

    Juntando dados e respostas

--- responderFicha/views.py
+++ responderFicha/views.py
@@ -48,18 +48,18 @@ def responderFicha(request):
 
         # Montando o dicionario para converter em DataFrame
         dadosFichas = {
-            'Modelo ficha': modeloFicha,
-            'Ficha':        tituloFicha
+            'Segue o modelo': modeloFicha,
+            'Cód. Ficha':        tituloFicha
             }
 
         # Transformando os dados em um DataFrame
         dadosFicha_df = pd.DataFrame(data=dadosFichas)
-        print("DataFrame da(s) fichas:\n", dadosFicha_df)
+        #print("DataFrame da(s) fichas:\n", dadosFicha_df)
 
         #print('Perguntas', perguntasDasFichas)
         respostasDasPerguntas = []
         for ficha in codFicha:
-            respostas = ['-'] * len(perguntasDasFichas)
+            respostas = [''] * len(perguntasDasFichas)
             #print("----Ficha:", ficha)
             dadosCat = db.child(tabelaBancoFicha).child(ficha).child('categorias').get().val()
             for cat in dadosCat:
@@ -78,18 +78,19 @@ def responderFicha(request):
 
         # Montando dataframe das respostas
         respostasDasPerguntas_df = pd.DataFrame(respostasDasPerguntas, columns=perguntasDasFichas)
-        print("Respostas das fichas:\n", respostasDasPerguntas_df)
+        #print("Respostas das fichas:\n", respostasDasPerguntas_df)
 
-        # Juntando Dados das fichas com as respostas
-
+        # Juntando dados das fichas com as respostas
+        csvFichas = pd.merge(dadosFicha_df, respostasDasPerguntas_df, left_index=True, right_index=True)
+        #print("DataFrame das fichas:\n", csvFichas)
 
         # Disponibilizando CSV para download
         responseCSV = HttpResponse(content_type='text/csv')
         responseCSV['Content-Disposition'] = 'attachment; filename=CSV_LabGRis ' + datetime.datetime.now().strftime('%d/%m/%Y') + '.csv'
-        dadosFicha_df.to_csv(path_or_buf=responseCSV, index=False)
+        csvFichas.to_csv(path_or_buf=responseCSV, index=False)
 
-        #return responseCSV
-        return redirect(pgCampo)
+        return responseCSV
+        #return redirect(pgCampo)
 
     return render(request, 'responderFicha/responderFicha.html', data)
 
